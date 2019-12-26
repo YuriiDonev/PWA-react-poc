9b1608cb083d784b3041bb0098f7253dea729ed2
Iurii Donev <diur@ciklum.com>
2019-12-26 09:07:05 +0200

    pwa 1

---
+++ precache-manifest.5a162c0bc333bb10a8bd317016b735ed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50171e6ebc9a8999823a2abf5a36478a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "61b2e176fcefc29a99df",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "83fcfc6fb5a11bb621a7",
+    "url": "/static/js/2.3584dbf7.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.3584dbf7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "61b2e176fcefc29a99df",
+    "url": "/static/js/main.7c058af7.chunk.js"
+  },
+  {
+    "revision": "bf38683fa9431d472ff8",
+    "url": "/static/js/runtime-main.17d8c657.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
